26c150f73e7005e889d23ba0bfc370255441fac8
RomainEtu <romain.ly.etu@gmail.com>
2021-04-20 23:14:37 +0200

    Setup Schedule.fxml and remove other things

--- java/application/App.java
+++ java/application/App.java
@@ -1,14 +1,11 @@
 package application;
 
-import controller.ScheduleController;
 import javafx.application.Application;
-import javafx.event.*;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import model.ScheduleModel;
-import view.ScheduleView;
+
+import java.io.IOException;
 
 public class App extends Application {
     public static void main(String [] args) {
@@ -16,13 +13,11 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
-        // Setup
-        ScheduleModel schedModel = new ScheduleModel();
-        ScheduleController schedController = new ScheduleController(schedModel);
-        ScheduleView schedView = new ScheduleView(schedController, schedModel);
+    public void start(Stage stage) throws IOException {
+        // Setup FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Schedule.fxml"));
 
-        stage.setScene(new Scene(schedView.AsParent(), 500, 500));
+        stage.setScene(new Scene(loader.load(), 500, 500));
         stage.show();
     }
 }
--- java/controller/ScheduleController.java
+++ java/controller/ScheduleController.java
@@ -20,7 +20,7 @@ public class ScheduleController {
     }
 
     // Getter
-    public ScheduleModel GetModel() {
+    private ScheduleModel GetModel() {
         return this.schedModel;
     }
 
