1de846e71e80f4bde636b1da841c95873ccdc16c
RomainEtu <romain.ly.etu@gmail.com>
2021-04-20 20:06:15 +0200

    Setup ScheduleMVC - Setter // Getter

--- java/controller/ScheduleController.java
+++ java/controller/ScheduleController.java
@@ -3,6 +3,25 @@ package controller;
 import model.ScheduleModel;
 
 public class ScheduleController {
+
+    private ScheduleModel schedModel;
+
     public ScheduleController(ScheduleModel schedModel) {
+        SetModel(schedModel);
     }
+
+    // Setter
+    private void SetModel(ScheduleModel schedModel) {
+        this.schedModel = schedModel;
+    }
+
+    public void SetSchedule(String input, String output, int dt, int ord, int bf) {
+        GetModel().getSchedule(input, output, dt, ord, bf);
+    }
+
+    // Getter
+    public ScheduleModel GetModel() {
+        return this.schedModel;
+    }
+
 }
